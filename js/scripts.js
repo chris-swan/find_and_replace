e9c8bbcabac6c2f0f309b0b20aedf4d95ffecca9
chris-swan <chris@chrisswan.com>
2015-09-11 13:02:07 -0700

    stuck on putting sentance back together, starting new approach at start of code

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
 var wordToFind = function(userPhrase, oldWord, newWord) {
 
   var words = userPhrase.split(" ");
-  var newWords = [];
+  var newWords = '';
   words.forEach(function(words) {
     if (words == oldWord) {
       var replacedWords = words.replace(oldWord, newWord)
-      newWords.push(newWord);
-    }
+      //newWords.push(newWord);
+    } else{ oldWord + 'does not appear in your phrase'}
   });
-  var result = newWords.join(" ");
-  // return newWords.join(" ");
-  return result;
+  //var result = newWords.join(" ");
+   return words;
 }
 
 $(document).ready(function() {
   $("form#wordToFind").submit(function(event) {
-    var user_phrase_form = String($("input#userPhrase").val());
-    var user_old_word_form = String($("input#oldWord").val());
-    var user_new_word_form = String($("input#newWord").val());
-    var result = wordToFind(user_phrase_form, user_old_word_form, user_new_word_form);
+    var user_phrase = String($("input#userPhrase").val());
+    var old_word = String($("input#oldWord").val());
+    var new_word = String($("input#newWord").val());
+    var result = wordToFind(user_phrase, old_word, new_word);
 
     $(".result").text(result);
 
